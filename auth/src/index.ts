534d8dc4836d1c3dbd4b22fd5473f9e38e26c6b8
Benjamin Chua <boyz_pro95@hotmail.com>
2021-02-02 18:10:38 +0800

    test auto deploy again

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting up...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
